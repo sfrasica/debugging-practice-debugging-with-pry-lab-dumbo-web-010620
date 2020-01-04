b1c11e955c6f275cdb3589addd53d711cc907851
Steven Frasica <sfrasica1@gmail.com>
2020-01-04 18:09:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -16,5 +16,3 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
-
-initialize
