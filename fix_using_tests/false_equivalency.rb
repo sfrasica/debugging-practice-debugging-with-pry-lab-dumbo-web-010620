c65215206fd72b8fa308181c76ada819958a8e9b
Steven Frasica <sfrasica1@gmail.com>
2020-01-04 18:11:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,11 +11,12 @@ def prompt_user
 end
 
 def selection(num)
-  binding.pry
+
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
+     binding.pry
   end
 end
 
